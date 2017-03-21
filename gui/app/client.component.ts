61f22a30bfb6d2bcfcdfa2c877a8abf6b7dff3c6
Tamás Bence Gedai <gedai.bence@gmail.com>
2017-03-21 11:30:35 +0100

    gui/app/client: Bootsrap added

--- gui/app/client.component.ts
+++ gui/app/client.component.ts
@@ -4,16 +4,32 @@ import { ClientService } from './client.service'
 @Component({
     selector: 'clients',
     template: `
-        <h2>Clients </h2>
-        <ul>
-            <li *ngFor='#client of clients'>
-                {{ client.name }}
-                {{ client.api_key }}
-                <a [routerLink]="['/Data', {id: client._id}]">Details</a>
-                <a [routerLink]="['/Config', {id: client._id}]">Config</a>
-            </li>
-        </ul>
-        `,
+    <style>
+    .nopadding {
+       padding: 0 !important;
+       margin: 10 !important;
+    }
+
+    .btn {
+        width: 49%;
+    }
+    </style>
+
+    <h2>Clients </h2>
+    <div class="container">
+        <div class="row">
+            <div class="panel panel-default col-lg-4 nopadding" *ngFor='#client of clients'>
+                <div class="panel-heading">
+                    <h4 class="panel-title"> {{ client.name }} </h4>
+                </div>
+                <div class="panel-body">
+                    <a class="btn btn-primary center" [routerLink]="['/Data', {id: client._id}]">Details</a>
+                    <a class="btn btn-primary center" [routerLink]="['/Config', {id: client._id}]">Config</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `,
     providers: [ClientService]
 })
 export class ClientComponent {
