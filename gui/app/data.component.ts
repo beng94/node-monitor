7b23cf1f02d3ed49c8e58f47128d314810b1f8fc
Tamás Bence Gedai <gedai.bence@gmail.com>
2017-05-02 13:09:05 +0200

    gui: d3 example graph added

--- gui/app/data.component.ts
+++ gui/app/data.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {DataService} from './data.service';
 import { RouteParams} from 'angular2/router';
 
@@ -17,7 +17,9 @@ import { RouteParams} from 'angular2/router';
             }
         </style>
 
+
         <h2>Data tag</h2>
+		<svg></svg>
                 <div class="panel panel-primary nopadding">
                     <table class="table table-hover">
                         <thead>
@@ -39,7 +41,7 @@ import { RouteParams} from 'angular2/router';
         `,
     providers: [DataService]
 })
-export class DataComponent {
+export class DataComponent implements OnInit {
     datas;
     JSON;
 
@@ -76,5 +78,68 @@ export class DataComponent {
                 });
             }
         );
+    }
+
+    ngOnInit() {
+        var data = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 7],
+        w = 400,
+        h = 200,
+        margin = 20,
+        y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
+        x = d3.scale.linear().domain([0, data.length]).range([0 + margin, w - margin])
+
+        var vis = d3.select("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        var g = vis.append("svg:g")
+            .attr("transform", "translate(0, 200)");
+
+        var line = d3.svg.line()
+            .x(function(d,i) { return x(i); })
+            .y(function(d) { return -1 * y(d); })
+
+        g.append("svg:path")
+            .attr("d", line(data))
+            .attr("fill", "none")
+            .attr("stroke", "blue";
+
+        g.selectAll(".xLabel")
+            .data(x.ticks(5))
+            .enter().append("svg:text")
+            .attr("class", "xLabel")
+            .text(String)
+            .attr("x", function(d) { return x(d) })
+            .attr("y", 0)
+            .attr("text-anchor", "middle")
+
+        g.selectAll(".yLabel")
+            .data(y.ticks(4))
+            .enter().append("svg:text")
+            .attr("class", "yLabel")
+            .text(String)
+            .attr("x", 0)
+            .attr("y", function(d) { return -1 * y(d) })
+            .attr("text-anchor", "right")
+            .attr("dy", 4)
+
+
+        g.selectAll(".xTicks")
+            .data(x.ticks(5))
+            .enter().append("svg:line")
+            .attr("class", "xTicks")
+            .attr("x1", function(d) { return x(d); })
+            .attr("y1", -1 * y(0))
+            .attr("x2", function(d) { return x(d); })
+            .attr("y2", -1 * y(-0.3))
+
+        g.selectAll(".yTicks")
+            .data(y.ticks(4))
+            .enter().append("svg:line")
+            .attr("class", "yTicks")
+            .attr("y1", function(d) { return -1 * y(d); })
+            .attr("x1", x(-0.3))
+            .attr("y2", function(d) { return -1 * y(d); })
+            .attr("x2", x(0))
     }
 }
